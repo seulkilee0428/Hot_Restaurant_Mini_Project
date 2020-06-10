2eb4afcc3d3946c3b89d40695b45619ab2a9ed86
Seulki Lee <seulki.lee.0428@gmail.com>
2020-06-09 20:34:59 -0500

    updating server.js

--- server.js
+++ server.js
@@ -1,7 +1,39 @@
 var express = require("express");
-var app = express();
+var path = require("path");
 
-var PORT = process.env.PORT || 8080;
+var app = express();
+var PORT = process.env.PORT || 3000;
+var reservations = [];
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "home.html"));
+});
+
+app.get("/reservation", function (req, res) {
+    res.sendFile(path.join(__dirname, "reservation.html"));
+});
+
+app.get("/tables", function (req, res) {
+    res.sendFile(path.join(__dirname, "tables.html"));
+});
+
+
+app.post("/api/tables", function (req, res) {
+    var newres = req.body;
+    console.log(newres);
+    reservations.push(newres);
+
+});
+
+app.get("/api/tables", function (req, res) {
+    console.log(reservations);
+    res.json(reservations);
+
+});
 
 app.listen(PORT, function () {
     console.log("App listening on PORT: " + PORT);
